781111dc84dbbd041d1de452eea689314c4b3238
amirzenoozi <amirzenoozi72@gmail.com>
2024-08-24 14:27:22 +0200

    :bug: Fix: Solve Env Issue

--- frontend/src/modules/apolloClient.ts
+++ frontend/src/modules/apolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-const API_URI: string = process.env.API_URL ?? 'http://localhost:3200/graphql';
+const API_URI: string = process.env.REACT_APP_API_URL ?? 'http://localhost:3200/graphql';
 const client = new ApolloClient({
 	link: new HttpLink({
 		uri: API_URI,
