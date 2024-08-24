0c0bbb9940de7288d9816c1c16df5df7be557b60
amirzenoozi <amirzenoozi72@gmail.com>
2024-08-24 11:17:52 +0200

    :bug: Fix: Solve CORS Issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3200);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: ['http://localhost:3000'],
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+	});
+	await app.listen(3200);
 }
+
 bootstrap();
