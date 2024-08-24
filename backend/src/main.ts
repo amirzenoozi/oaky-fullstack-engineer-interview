ec7ee90967a346f71600254fb4eabf7020fb4214
amirzenoozi (Hetzner) <amirzenoozi72@gmail.com>
2024-08-24 16:22:09 +0000

    :bug: Fix: Solve The CORS Issue on the Server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: ['http://localhost:3000', 'https://oakyb.amirdouzandeh.me'],
-		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		origin: 'https://oakyf.amirdouzandeh.me',
+		methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
 		credentials: true,
+		preflightContinue: false,
+		optionsSuccessStatus: 204,
 	});
 	await app.listen(3200);
 }
