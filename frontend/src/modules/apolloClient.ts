79935cea54ca5ebce4456b2064c6b50ffb94b1fd
amirzenoozi <amirzenoozi72@gmail.com>
2024-08-24 15:06:39 +0200

    :bug: Fix: Adding Debug Tools

--- frontend/src/modules/apolloClient.ts
+++ frontend/src/modules/apolloClient.ts
@@ -1,5 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
+console.log('API URL:', process.env.REACT_APP_API_URL);
+
 const API_URI: string = process.env.REACT_APP_API_URL ?? 'http://localhost:3200/graphql';
 const client = new ApolloClient({
 	link: new HttpLink({
