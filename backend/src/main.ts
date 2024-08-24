c63b9455db23409f18ada12f77ff4b6879c767ae
amirzenoozi <amirzenoozi72@gmail.com>
2024-08-24 15:19:11 +0200

    :bug: Fix: Handle CORS Issue on the Server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: ['http://localhost:3000'],
+		origin: ['http://localhost:3000', 'http://*.amirdouzandeh.me/'],
 		methods: ['GET', 'POST', 'PUT', 'DELETE'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
 	});
