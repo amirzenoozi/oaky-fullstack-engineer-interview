5a2b345a5081d6cac917f3d50a9a53623bf92027
amirzenoozi <amirzenoozi72@gmail.com>
2024-08-24 21:36:13 +0200

    :zap: Feat: Get ALLOWED_ORIGIN From Env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
 
+const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: 'https://oakyf.amirdouzandeh.me',
+		origin: ALLOWED_ORIGIN,
 		methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
 		credentials: true,
