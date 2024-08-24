5d75d3c89cd7d5d2241de7fc2ba3fec6752f502f
amirzenoozi <amirzenoozi72@gmail.com>
2024-08-24 11:16:24 +0200

    :bug: Fix: Solve Directory Issue

--- runner.sh
+++ runner.sh
@@ -1,3 +1,3 @@
 # Run Both Front and Backed Application at the same time
 echo "Running Veriff Interview Application..."
-(cd front && yarn run start &) && (cd back && yarn start:dev)
+(cd frontend && yarn run start &) && (cd backend && yarn start:dev)
